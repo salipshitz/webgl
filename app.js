139ab982c1d753f46229219f9ecf241654fbeb12
Sa'ar <geger40.1@gmail.com>
2018-10-13 13:29:28 -0700

    Add clear color

--- app.js
+++ app.js
@@ -3,4 +3,7 @@ function Start() {
   const gl = canvas.getContext('webgl');
   if (gl === null) gl = canvas.getContext('experimental-webgl');
   if (gl === null) alert('Your browser does not support WebGL');
+  
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear();
 }
