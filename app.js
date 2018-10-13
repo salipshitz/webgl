80d10d729cdb23e411b9fe58312b80d87c0944c8
Sa'ar <geger40.1@gmail.com>
2018-10-13 13:27:41 -0700

    Setup WebGL

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+function Start() {
+  const canvas = document.getElementById('triangle-canvas');
+  const gl = canvas.getContext('webgl');
+  if (gl === null) gl = canvas.getContext('experimental-webgl');
+  if (gl === null) alert('Your browser does not support WebGL');
+}
