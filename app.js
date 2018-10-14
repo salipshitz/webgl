57ba3177e8b94221c05d440e70ec14387639eab0
Sa'ar <geger40.1@gmail.com>
2018-10-13 21:44:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -190,7 +190,6 @@ var Start = function () {
 	//
 	// Create texture
 	//
-	const images = [document.getElementById('brick-image'), document.getElementById('brick1-image')];
 	const boxTexture = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, boxTexture);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -200,7 +199,7 @@ var Start = function () {
 	gl.texImage2D(
 		gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA,
 		gl.UNSIGNED_BYTE,
-		images[1]
+		document.getElementById('question-image')
 	);
 	gl.bindTexture(gl.TEXTURE_2D, null);
 
