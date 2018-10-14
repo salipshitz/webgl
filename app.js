109838f831d6dd1a5c2db5eaf67356c526691202
Sa'ar <geger40.1@gmail.com>
2018-10-13 21:34:48 -0700

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var fragmentShaderText = [
 'precision mediump float;',
 
 'varying vec2 fragTexCoord;',
-'uniform sampler2D brick;',
+'uniform sampler2D sampler;',
 
 'void main() {',
   'gl_FragColor = texture2D(sampler, fragTexCoord);',
